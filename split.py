a2dbc1e19ea9fe2ceecd106cb80d7c6c19988aad
Chris Field <94464835+crfield18@users.noreply.github.com>
2023-09-29 15:17:00 +0100

    ' swapped for " for uniformity between scripts

--- split.py
+++ split.py
@@ -1,11 +1,11 @@
-'''Determine the number of monovalent salt ions are required
+"""Determine the number of monovalent salt ions are required
 for an explicit solvent MD simulation using the SPLIT method
 (http://archive.ambermd.org/202002/0194.html)
 
 No = Expected number of salt ions
 Co = Salt concentration (M)
 Nw = Number of water molecules
-Q  = Solute's charge'''
+Q  = Solute's charge"""
 
 from math import ceil
 
@@ -15,31 +15,31 @@ Input_Nw = 6817
 Input_Q = 0
 
 def is_even(num:int):
-    '''Checks whether the input num is even or odd.'''
+    """Checks whether the input num is even or odd."""
     if (num % 2) == 0:
         return True
     return False
 
 def split_valid(No:float, Q:float):
-    '''Checks if the SPLIT method is valid for the provided data set.
-    It is only valid when No/Q ≥ 1'''
+    """Checks if the SPLIT method is valid for the provided data set.
+    It is only valid when No/Q ≥ 1"""
     if Q == 0 or No/Q >= 1:
         return True
     return False
 
 def split(Nw:float, Co:float, Q:float):
-    '''Calculates the number of salt ions needed'''
+    """Calculates the number of salt ions needed"""
     # if is_even(Q) == False:
     #     Q += 1
     No = Nw*(Co/56)
     if not split_valid(No, Q):
-        return 'SPLIT not valid for this dataset (No/Q < 1)'
+        return "SPLIT not valid for this dataset (No/Q < 1)"
 
     nplus = ceil(No - (Q/2))
     nminus = ceil(No + (Q/2))
 
     return nplus, nminus
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     nplus, nminus = split(Nw=Input_Nw, Co=Input_Co, Q=Input_Q)
-    print(f'addions2 pdb K+ {nplus} Cl- {nminus}')
+    print(f"addions2 pdb K+ {nplus} Cl- {nminus}")
