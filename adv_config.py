bdaa9f5061dfc71ca864251d85a875498b5686ae
Chris Field <94464835+crfield18@users.noreply.github.com>
2024-01-15 16:50:59 +0000

    swapped " for ' for consistency between scripts

--- adv_config.py
+++ adv_config.py
@@ -1,4 +1,4 @@
-"""
+'''
 Generates a template config file for use with AutoDock Vina/Vina-Carb
 using a grid dimensions file generated with AutoDockTools.
 
@@ -7,26 +7,26 @@ Args:
 
 Returns:
     File named "config.txt" in the same directory as the input file
-"""
+'''
 
 from argparse import ArgumentParser
 import pathlib
 
 def script_args():
-    """
+    '''
     Parse command-line arguments and return a Namespace object containing
     pathlib.Path paths to each user-defined input file.
 
     Returns:
         argparse.Namespace: A Namespace object containing input file paths.
-    """
+    '''
     parser = ArgumentParser()
-    parser.add_argument("-i", "--input", type=pathlib.Path, required=True, nargs="+",
-                        help="AutoDockTools gridbox output files")
+    parser.add_argument('-i', '--input', type=pathlib.Path, required=True, nargs='+',
+                        help='AutoDockTools gridbox output files')
     return parser.parse_args()
 
 def parse_gridbox(gridbox_file):
-    """
+    '''
     Converts each user-defined input file into a template AutoDock Vina config file.
 
     Args:
@@ -34,56 +34,56 @@ def parse_gridbox(gridbox_file):
 
     Returns:
         None
-    """
+    '''
 
     gridbox_contents = []
     index_npts = None
     index_center = None
 
-    with open(gridbox_file, "r", encoding="utf-8") as input_file:
+    with open(gridbox_file, 'r', encoding='utf-8') as input_file:
         gridbox_contents = [line.split() for line in input_file if line.strip()]
 
     for index, item in enumerate(gridbox_contents):
-        if item[0] == "npts":
+        if item[0] == 'npts':
             index_npts = index
-        elif item[0] == "center":
+        elif item[0] == 'center':
             index_center = index
 
     variable_mappings = [
-        ("center_x", 1),
-        ("center_y", 2),
-        ("center_z", 3),
-        ("size_x", 1),
-        ("size_y", 2),
-        ("size_z", 3),
+        ('center_x', 1),
+        ('center_y', 2),
+        ('center_z', 3),
+        ('size_x', 1),
+        ('size_y', 2),
+        ('size_z', 3),
     ]
 
-    with open(gridbox_file.with_name("config.txt"), "w", encoding="utf-8") as file:
-        file.writelines("receptor = \nligand = \n\n")
+    with open(gridbox_file.with_name('config.txt'), 'w', encoding='utf-8') as file:
+        file.writelines('receptor = \nligand = \n\n')
 
         for variable, index in variable_mappings:
-            if variable.startswith("center"):
+            if variable.startswith('center'):
                 data_source = gridbox_contents[index_center]
-            elif variable.startswith("size"):
+            elif variable.startswith('size'):
                 data_source = gridbox_contents[index_npts]
 
-            file.writelines(f"{variable} = {data_source[index]}\n")
+            file.writelines(f'{variable} = {data_source[index]}\n')
 
-        file.writelines("\nenergy_range = 4\nexhaustiveness = 32\n")
-        file.writelines("\nlog = \nout = \n")
+        file.writelines('\nenergy_range = 4\nexhaustiveness = 32\n')
+        file.writelines('\nlog = \nout = \n')
 
 def main():
-    """
+    '''
     Primary execution point of the script.
 
     Usage:
         To run this script, execute it from the command line as follows:
         $ python3 adv-config.py -i [path/to/file]
-    """
+    '''
 
     user_args = script_args()
     for file in user_args.input:
         parse_gridbox(file)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
--- split.py
+++ split.py
@@ -1,4 +1,4 @@
-"""
+'''
 Determine the number of monovalent salt ions are required
 for an explicit solvent MD simulation using the SPLIT method.
 See: http://archive.ambermd.org/202002/0194.html
@@ -14,7 +14,7 @@ Returns:
     num_minus = number of negatively charged ions (Cl-).
 
     For use with LEaP (AmberTools).
-"""
+'''
 
 from math import ceil
 
@@ -24,7 +24,7 @@ input_nw = 6817
 input_q = 0
 
 def is_even(num):
-    """
+    '''
     Check whether the input number is even or odd.
 
     Args:
@@ -32,13 +32,13 @@ def is_even(num):
 
     Returns:
         bool: True if the number is even, False if it's odd.
-    """
+    '''
     if (num % 2) == 0:
         return True
     return False
 
 def split_valid(num_salt, charge_solute):
-    """
+    '''
     Check if the SPLIT method is valid for the provided data set.
     It is only valid when num_salt/charge_solute ≥ 1.
 
@@ -48,13 +48,13 @@ def split_valid(num_salt, charge_solute):
 
     Returns:
         bool: True if the system is valid, False otherwise.
-    """
+    '''
     if charge_solute == 0 or num_salt/charge_solute >= 1:
         return True
     return False
 
 def split(num_solv, conc_salt, charge_solute):
-    """
+    '''
     Calculate the number of salt ions needed for a valid system.
 
     Args:
@@ -65,16 +65,16 @@ def split(num_solv, conc_salt, charge_solute):
     Returns:
         tuple: A tuple containing the number of positively charged ions (K+) and
         the number of negatively charged ions (Cl-).
-    """
+    '''
     num_salt = num_solv * (conc_salt / 56)
     if not split_valid(num_salt, charge_solute):
-        return "SPLIT not valid for this dataset (num_salt/charge_solute < 1)"
+        return 'SPLIT not valid for this dataset (num_salt/charge_solute < 1)'
 
     num_plus = ceil(num_salt - (charge_solute / 2))
     num_minus = ceil(num_salt + (charge_solute / 2))
 
     return num_plus, num_minus
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     num_k, num_cl = split(num_solv=input_nw, conc_salt=input_co, charge_solute=input_q)
-    print(f"addions2 pdb K+ {num_k} Cl- {num_cl}")
+    print(f'addions2 pdb K+ {num_k} Cl- {num_cl}')
