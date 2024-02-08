0489be1470cc2bba32781786296b483955fe6fa9
chris-homelinux <94464835+crfield18@users.noreply.github.com>
2024-02-08 00:55:46 +0000

    added cealign and super

--- align_pdb.py
+++ align_pdb.py
@@ -58,35 +58,94 @@ def align_structure():
     # Define the reference structure for alignment.
     # Defaults to the first pdb file from the inputs list
     ref_pdb_path = user_args.ref if user_args.ref else pdb_files[0]
-    ref_pdb = ref_pdb_path.stem.replace(" ", "_")
+    ref_pdb = ref_pdb_path.stem.replace(' ', '_')
 
     # Add reference structure to list of pdb files if it's not already
     if user_args.ref and user_args.ref not in pdb_files:
         pdb_files.append(user_args.ref)
 
+    # Create a pymol command log file
+    pymol_session(f'log_open {ref_pdb}_aligned.log')
+
     # Load each pdb file
+    pymol_session(f'load {ref_pdb_path}')
+
     for pdb in pdb_files:
         pymol_session(f'load {pdb}')
 
     # Show all structures using cartoon representation
     pymol_session('hide all')
+    pymol_session(f'set cartoon_transparency, 0.90,all')
     pymol_session('show cartoon,all')
+    pymol_session(f'set cartoon_transparency, 0,{ref_pdb}')
 
     # Centre view on the reference structure
     pymol_session(f'center {ref_pdb}')
 
     # Align each structure to the reference structure
     for pdb in pdb_files:
-        current_pdb = pdb.stem.replace(" ", "_")
+        current_pdb = pdb.stem.replace(' ', '_')
         if current_pdb != ref_pdb:
+            pymol_session(f'set cartoon_transparency, 0, {current_pdb}')
+            # align is better for sequentially similar proteins
+            # super or cealign is better for structurally similar proteins
             pymol_session(f'align {current_pdb}, {ref_pdb}')
+            if user_args.gui:
+                pymol_session('zoom all')
+            pymol_session(f'set cartoon_transparency, 0.90, {current_pdb}')
         else:
             pass
 
+    # Remove all water molecules
+    pymol_session('remove resn hoh')
+
+    # Center viewport view on the reference structure
+    pymol_session(f'center {ref_pdb}')
+    pymol_session(f'zoom {ref_pdb}')
+
     # Save current PyMOL session as a .pse file
-    output_file = ref_pdb_path.parent / f'{ref_pdb}_aligned.pse'
+    output_file = ref_pdb_path.parent / f'{ref_pdb}_align.pse'
     pymol_session(f'save {output_file}')
     print(f'Output file: {output_file}')
+
+    for pdb in pdb_files:
+        current_pdb = pdb.stem.replace(' ', '_')
+        if current_pdb != ref_pdb:
+            pymol_session(f'set cartoon_transparency, 0, {current_pdb}')
+            pymol_session(f'cealign {ref_pdb}, {current_pdb}')
+            if user_args.gui:
+                pymol_session('zoom all')
+            pymol_session(f'set cartoon_transparency, 0.90, {current_pdb}')
+        else:
+            pass
+
+    pymol_session(f'center {ref_pdb}')
+    pymol_session(f'zoom {ref_pdb}')
+
+    output_file = ref_pdb_path.parent / f'{ref_pdb}_cealign.pse'
+    pymol_session(f'save {output_file}')
+    print(f'Output file: {output_file}')
+
+    for pdb in pdb_files:
+        current_pdb = pdb.stem.replace(' ', '_')
+        if current_pdb != ref_pdb:
+            pymol_session(f'set cartoon_transparency, 0, {current_pdb}')
+            pymol_session(f'super {current_pdb}, {ref_pdb}')
+            if user_args.gui:
+                pymol_session('zoom all')
+            pymol_session(f'set cartoon_transparency, 0.90, {current_pdb}')
+        else:
+            pass
+
+    pymol_session(f'center {ref_pdb}')
+    pymol_session(f'zoom {ref_pdb}')
+
+    output_file = ref_pdb_path.parent / f'{ref_pdb}_super.pse'
+    pymol_session(f'save {output_file}')
+    print(f'Output file: {output_file}')
+
+
+    pymol_session('log_close')
 
 if __name__ == '__main__':
     align_structure()
