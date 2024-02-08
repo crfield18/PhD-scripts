ef7c2797965669e6d90ee9801876fd184888078a
Chris Field <94464835+crfield18@users.noreply.github.com>
2024-02-08 13:52:56 +0000

    testing loading pymol scripts from github

---
+++ pymol/showaromatics.py
@@ -0,0 +1,14 @@
+from pymol import cmd
+
+def showaromatics(selection='all'):
+    cmd.hide('everything', selection)
+    cmd.show('cartoon', selection)
+    cmd.color('gray', selection)
+    cmd.select('aromatics', f'(resn tyr+trp+his) and {selection}')
+    cmd.show('sticks', f'(aromatics and !(name c+n+o))')
+    cmd.color('magenta', 'aromatics')
+    cmd.disable('aromatics')
+    cmd.set('cartoon_smooth_loops', 0)
+
+cmd.extend('showaromatics', showaromatics)
+cmd.auto_arg[0]['showaromatics'] = [cmd.selection_sc, 'selection', '']
---
+++ pymol/test.pml
@@ -0,0 +1,2 @@
+select all
+color red, all
