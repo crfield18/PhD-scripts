72fac48b2eb5faa9dddb6da425ea3c035bf014e5
Chris Field <94464835+crfield18@users.noreply.github.com>
2024-02-08 16:16:58 +0000

    initial commit

---
+++ pymol/align_all.py
@@ -0,0 +1,42 @@
+from pymol import cmd
+
+def non_sele_objs():
+    # Get a list of every object present in the current session (excluding 'sele')
+    all_objects = cmd.get_object_list()
+    non_sele_obj_list = [obj for obj in all_objects if obj != 'sele']
+    return non_sele_obj_list
+
+def align_to_ref(reference_object:str, method:str):
+    # Centre viewport on reference object
+    cmd.center(reference_object)
+
+    # Assign method strings to pymol commands
+    alignment_functions = {
+        'align': cmd.align,
+        'cealign': cmd.cealign,
+        'super': cmd.super
+        # # Requires TMalign executable. See: https://pymolwiki.org/index.php/TMalign
+        # 'tmalign': cmd.tmalign
+    }
+
+    # Run command for each object (excluding 'sele' and the chosen reference object)
+    method_function = alignment_functions.get(method)
+    if method_function:
+        for obj in non_sele_objs():
+            if obj != reference_object:
+                method_function(obj, reference_object)
+    else:
+        pass
+
+# Create callable function inside pymol called method_all for each alignment algorithm
+def define_alignment_method(method_str):
+    def align_all(reference_object):
+        align_to_ref(reference_object, method=method_str)
+    # function accepts 1 object to be used as the reference
+    cmd.extend(f'{method_str}_all', align_all)
+    cmd.auto_arg[0][f'{method_str}_all'] = [cmd.object_sc, 'object', '']
+
+alignment_methods = ['align', 'cealign', 'super']
+
+for algorithm in alignment_methods:
+    define_alignment_method(algorithm)
