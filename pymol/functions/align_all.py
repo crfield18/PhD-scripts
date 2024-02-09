c8f7f51df19a21e88ba74e1a6279d7f4f1006023
Chris Field <94464835+crfield18@users.noreply.github.com>
2024-02-09 17:01:13 +0000

    added csv output. testing tmalign

--- pymol/functions/align_all.py
+++ pymol/functions/align_all.py
@@ -1,11 +1,16 @@
+from pathlib import Path
+import csv
 from pymol import cmd
 
+cwd = Path.cwd()
+
+# Get a list of every object present in the current session (excluding 'sele')
 def non_sele_objs():
-    # Get a list of every object present in the current session (excluding 'sele')
     all_objects = cmd.get_object_list()
     non_sele_obj_list = [obj for obj in all_objects if obj != 'sele']
     return non_sele_obj_list
 
+# Align each object (excluding 'sele') to reference_object using the specified method
 def align_to_ref(reference_object:str, method:str):
     # Centre viewport on reference object
     cmd.center(reference_object)
@@ -19,33 +24,43 @@ def align_to_ref(reference_object:str, method:str):
         # 'tmalign': cmd.tmalign
     }
 
+    results_dict = {}
+
     # Run command for each object (excluding 'sele' and the chosen reference object)
     method_function = alignment_functions.get(method)
+
     if method_function:
         print(f'\nReference object:\t{reference_object}\n')
         for obj in non_sele_objs():
             if obj != reference_object:
+                # Extract the RMSD value from the different result types
                 results = method_function(obj, reference_object)
-                # super and align
-                if type(results) == tuple:
+                if method in ('super', 'align'):
                     rmsd = results[0]
-                # cealign
-                elif type(results) == dict:
+                elif method == 'cealign':
                     rmsd = results["RMSD"]
-                print(f'{obj}:\t{rmsd:.5f} Å')
-    else:
-        pass
+                else:
+                    print(rmsd)
+                print(f'\r{obj}:\t{rmsd:.5f} Å    ', end='', flush=True)
+                results_dict[obj] = rmsd
+
+    # Write the alignment results of a csv file in the current working directory
+    with open(cwd / f'{reference_object}-{method}.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['target', f'{method}_rmsd'])
+        for key, value in results_dict.items():
+            writer.writerow([key, value])
+
+    print(f'\nResults written to:\t{reference_object}-{method}.csv\n')
 
 # Create callable function inside pymol called method_all for each alignment algorithm
 def define_alignment_method(method_str):
     def align_all(reference_object):
         align_to_ref(reference_object, method=method_str)
-        print(f'{method_str}_all complete!')
+
     # function accepts 1 object to be used as the reference
     cmd.extend(f'{method_str}_all', align_all)
     cmd.auto_arg[0][f'{method_str}_all'] = [cmd.object_sc, 'object', '']
 
-alignment_methods = ['align', 'cealign', 'super']
-
-for algorithm in alignment_methods:
+for algorithm in ['tmalign', 'align', 'cealign', 'super']:
     define_alignment_method(algorithm)
