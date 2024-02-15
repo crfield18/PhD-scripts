2f27d042f695baf8abe81371ec557fac739d9732
Chris Field <94464835+crfield18@users.noreply.github.com>
2024-02-15 17:13:30 +0000

    reorganised functions; import order. added list of jobs at bottom

--- pymol/align_models.py
+++ pymol/align_models.py
@@ -1,16 +1,13 @@
+import datetime
+from itertools import permutations
+import json
+from math import isnan
 from pathlib import Path
 from argparse import ArgumentParser
-import json
-# Figure out how to only use pandas
 import numpy as np
 import pandas as pd
 from pymol import cmd
 import psico.fullinit
-import datetime
-from math import isnan
-from itertools import permutations
-
-results_dict = {}
 
 class PyMOLRMS():
     def __init__(self, rms_results) -> None:
@@ -57,13 +54,25 @@ class PyMOLcealign():
         # Rotation matrix
         self.rotation_matrix = np.array(self.ce_results['rotation_matrix'])
 
+    def show_results(self):
+        print(f'''
+RMSD after refinement:\t\t\t{self.rmsd}
+# of aligned atoms after refinement:\t{self.nalign}
+Model Rotation Matrix:
+''')
+
+    @property
+    def rmsd(self):
+        return self.rmsd
+
 class PyMOLAlign():
     def __init__(self, reference_object:str) -> None:
         self.ref = reference_object
         self.obj_list = PyMOLAlign.non_sele_objs()
         self.results_dict = {}
 
-    def non_sele_objs():
+    def non_sele_objs(self):
+        # Get a list of all object loaded into PyMOL, excluding 'sele'
         all_objects = cmd.get_object_list()
         non_sele_obj_list = [obj for obj in all_objects if obj != 'sele']
         return non_sele_obj_list
@@ -91,8 +100,9 @@ class PyMOLAlign():
             if obj != self.ref:
                 print(f'\nAligning based on structure (cealign):\t{obj}')
                 rms = cmd.cealign(self.ref, obj)
-                print(rms)
-                sub_results_dict = {'cealign': [rms['RMSD'], rms['alignment_length']]}
+                cealign_result_parser = PyMOLcealign(rms)
+                cealign_result_parser.show_results()
+                sub_results_dict = {'cealign': [cealign_result_parser.rmsd, cealign_result_parser.nalign]}
                 self.results_dict.setdefault(obj, {}).update(sub_results_dict)
 
     def super_all(self):
@@ -105,17 +115,48 @@ class PyMOLAlign():
                                               align_result_parser.nalign_res]}
                 self.results_dict.setdefault(obj, {}).update(sub_results_dict)
 
+    def results_dict_to_df(self):
+        # Convert the alignment results dictionary to a dataframe
+        if not self.df:
+            self.df = pd.DataFrame.from_dict(self.results_dict, orient='index')
+            # Expand lists and assign elements to new columns
+            self.df[['align_RMSD', 'align_nres']] = self.df['align'].apply(pd.Series)
+            self.df[['cealign_RMSD', 'cealign_nres']] = self.df['cealign'].apply(pd.Series)
+            self.df[['super_RMSD', 'super_nres']] = self.df['super'].apply(pd.Series)
+
+            # Drop the original columns with lists
+            self.df.rename(columns={'tmalign': 'TM-align'}, inplace=True)
+            self.df.drop(['align', 'cealign', 'super'], axis=1, inplace=True)
+
+        return self.df
+
+    def df_to_csv(self, dataframe:pd.DataFrame, filename:str, index_label:str=None):
+        # Write alignment results dataframe to a csv file
+        current_date = datetime.datetime.now().strftime('%Y%m%d')
+        if index_label:
+            dataframe.to_csv(f'{filename}-{current_date}.csv', index=True, index_label=index_label)
+        else:
+            dataframe.to_csv(f'{filename}-{current_date}.csv', index=True)
+
+    def df_to_xlsx(self, dataframe:pd.DataFrame, filename:str, index_label:str=None):
+        # Write alignment results dataframe to a xlsx file
+        # Add second sheet with reminder text here
+        current_date = datetime.datetime.now().strftime('%Y%m%d')
+        if index_label:
+            dataframe.to_csv(f'{filename}-{current_date}.xlsx', index=True, index_label=index_label)
+        else:
+            dataframe.to_csv(f'{filename}-{current_date}.xlsx', index=True)
+
     def tm_matrix(self):
-
-
-        # Used for checking duplicates
-        unique_model_set = set()
-        unique_model_list = []
+        # Generate a matrix of TMalign scores to build a structural dendrogram
 
         # Create a list of unique PDB codes ordered by their TMalign score
+        unique_model_set = set()
+        unique_model_list = []
         model_list = [model for model, results in self.results_dict.items()]
         for m in model_list:
-            if len(unique_model_list) < 25:
+            # 100 models max to limit the numner of permutations
+            if len(unique_model_list) < 100:
                 # Could check for duplicates of first 2 letters of pdb code to find proteins from different sources
                 if m.split('-')[0] not in unique_model_set:
                     unique_model_set.add(m.split('-')[0])
@@ -135,20 +176,14 @@ class PyMOLAlign():
             if tm > self.tmmatrix.loc[pair[0], pair[1]] or isnan(self.tmmatrix.loc[pair[0], pair[1]]):
                 self.tmmatrix.loc[pair[0], pair[1]] = tm
                 self.tmmatrix.loc[pair[1], pair[0]] = tm
-            PyMOLAlign.tm_matrix_show(self)
 
+        # Fill the diagonal with TMscores of 1
+            # Identical proteins give a TMscore of 1
+            # ∴ We do not need to run any calculations
         for obj in unique_model_list:
             self.tmmatrix.loc[obj,obj] = 1.0
+
         return self.tmmatrix
-    
-    def tm_matrix_show(self):
-        print(self.tmmatrix)
-    
-    def tm_matrix_to_csv(self):
-        # csv_file = 'tmalign_matrix.csv'
-        self.tmmatrix.to_csv('tmalign_matrix.csv', index=True)
-        # return csv_file
-    
 
 def script_args():
     parser = ArgumentParser()
@@ -158,7 +193,65 @@ def script_args():
                         help='Path to directory containing all pdb/cif files to align to the reference structure.')
     return parser.parse_args()
 
+def align():
+    user_args = script_args()
+
+    # Get a list of all the files in the user specified directory
+    test_dir_path = user_args.directory.resolve()
+    cif_list = [file for file in test_dir_path.iterdir()
+                if file.is_file() and file.suffix in ('.pdb', '.cif')]
+
+    # Get the path for the user specified reference pdb/cif file
+    cif_ref_path = user_args.reference.resolve()
+
+    # Load all pdb/cif files in cwd
+    if cif_ref_path not in cif_list:
+        cmd.load(cif_ref_path)
+    for cif in cif_list:
+        print(f'Loading: {cif}')
+        cmd.load(cif)
+
+    # Remove waters
+    cmd.remove('resn hoh')
+
+    # Initialise a PyMOL instance and set the reference object
+    pymol_instance = PyMOLAlign(cif_ref_path.stem)
+
+    # Align all other objects to the reference using align, cealign, super and tmalign
+    for alignment_algorithm in (pymol_instance.align_all,
+                                pymol_instance.cealign_all,
+                                pymol_instance.super_all,
+                                pymol_instance.tmalign_all):
+        print('-' * 100)
+        alignment_algorithm()
+
+    # Write results to a JSON file (mainly for testing)
+    with open('results.json', 'w', encoding='UTF8') as json_file:
+        results_sorted_by_tm = dict(sorted(pymol_instance.results_dict.items(),
+                                           key=lambda item: item[1]["tmalign"], reverse=True))
+        json.dump(results_sorted_by_tm, json_file, indent=4)
+
+    # Convert the results dictionary to a dataframe and write to csv and xlsx files
+    pymol_instance.results_dict_to_df()
+    pymol_instance.df_to_csv(dataframe=pymol_instance.df,
+                             filename=f'{pymol_instance.ref}-alignment-',
+                             index_label='PDB_code_with_chain')
+    pymol_instance.df_to_xlsx(dataframe=pymol_instance.df,
+                             filename=f'{pymol_instance.ref}-alignment-',
+                             index_label='PDB_code_with_chain')
+
+    # Calculate TMalign score matrix
+        # Used to create a structural dendrogram
+    pymol_instance.tm_matrix()
+    pymol_instance.df_to_xlsx(dataframe=pymol_instance.tmmatrix,
+                             filename=f'tmalign-matrix')
+
+    return pymol_instance.ref, pymol_instance.results_dict
+
 def visualise_top_x(results:dict, top_x:int, reference_obj:str):
+    # Create PyMOL sessions for the top x PDBs with the highest TMalign scores
+    # Only 1 chain per PDB code is used (often there are identical chains in the same PDB) 
+
     # Used for checking duplicates
     unique_model_set = set()
     unique_model_list = []
@@ -193,83 +286,28 @@ def visualise_top_x(results:dict, top_x:int, reference_obj:str):
         cmd.super(f'{m}_super', reference_obj)
         cmd.tmalign(f'{m}_tmalign', reference_obj)
 
-
-        # Set cartoon transparency to 0.65 (35% visible)
+        # 0.65 = 35% visible
         cmd.set('cartoon_transparency', 0.65, f'(all and not {reference_obj})')
-
         cmd.center(reference_obj)
+        
         # Save pymol session file
         cmd.save(f'{m}.pse')
         cmd.reinitialize()
 
-def align():
-    user_args = script_args()
-
-    # Get a list of all the files in the user specified directory
-    test_dir_path = user_args.directory.resolve()
-    cif_list = [file for file in test_dir_path.iterdir()
-                if file.is_file() and file.suffix in ('.pdb', '.cif')]
-
-    # Get the path for the user specified reference pdb/cif file
-    cif_ref_path = user_args.reference.resolve()
-
-    # Load all pdb/cif files in cwd
-    if cif_ref_path not in cif_list:
-        cmd.load(cif_ref_path)
-    for cif in cif_list:
-        print(f'Loading: {cif}')
-        cmd.load(cif)
-
-
-    # Remove waters
-    cmd.remove('resn hoh')
-
-    pymol_instance = PyMOLAlign(cif_ref_path.stem)
-
-    for alignment_algorithm in (pymol_instance.align_all, pymol_instance.cealign_all, pymol_instance.super_all, pymol_instance.tmalign_all):
-        print('-' * 100)
-        alignment_algorithm()
-
-    # Write results to a JSON file (mainly for testing)
-    with open('results.json', 'w', encoding='UTF8') as json_file:
-        results_sorted_by_tm = dict(sorted(pymol_instance.results_dict.items(),
-                                           key=lambda item: item[1]["tmalign"], reverse=True))
-        json.dump(results_sorted_by_tm, json_file, indent=4)
-
-    pymol_instance.tm_matrix()
-    pymol_instance.tm_matrix_to_csv()
-
-    return pymol_instance.ref, pymol_instance.results_dict
-
-if __name__ == '__main__':
+def main():
     reference_obj, alignment_results = align()
-    with open('results.json', 'r') as results_json:
-        alignment_results = json.load(results_json)
-    reference_obj = 'ranked_0'
+    # with open('results.json', 'r', encoding='UTF8') as results_json:
+    #     alignment_results = json.load(results_json)
+
     visualise_top_x(alignment_results, 25, reference_obj)
 
-    # Write results to a csv file
-    df = pd.DataFrame.from_dict(alignment_results, orient='index')
+if __name__ == '__main__':
+    main()
 
-    print(df)
-    # Expand lists and assign elements to new columns
-    df[['align_RMSD', 'align_nres']] = df['align'].apply(pd.Series)
-    df[['cealign_RMSD', 'cealign_nres']] = df['cealign'].apply(pd.Series)
-    df[['super_RMSD', 'super_nres']] = df['super'].apply(pd.Series)
-
-    # Drop the original columns with lists
-    df.rename(columns={'tmalign': 'TM-align'}, inplace=True)
-    df.drop(['align', 'cealign', 'super'], axis=1, inplace=True)
-
-    # Print the DataFrame
-    print(df)
-
-    current_date = datetime.datetime.now().strftime('%Y%m%d')
-    df.to_csv(f'{reference_obj}-alignment-{current_date}.csv', index=True, index_label='PDB_code_with_chain')
-    df.to_excel(f'{reference_obj}-alignment-{current_date}.xlsx', index=True, index_label='PDB_code_with_chain')
-
-
-# Jobs for saturday:
+# Jobs for Saturday:
     # Lint Code
     # Separate results generation to its own function probably
     # Add reminder sheet to excel doc with basic info on each alignment algorithm/how to interpret scores
+    # name objects in visualise_top_x based on RMSD/TMalign score
+    # make visualise_top_x add column to alignment table that indicates which ones have a pymol session
+    # Move finding unique pdbs in a list to its own function (currently its copied twice)
