c68adde9d405653cd909c802bf1f62c004fb5cb1
chris-homelinux <94464835+crfield18@users.noreply.github.com>
2024-02-23 01:56:36 +0000

    wednesday update

--- pymol/align_models.py
+++ pymol/align_models.py
@@ -1,4 +1,5 @@
 import glob
+from tqdm import tqdm
 import json
 import datetime
 from argparse import ArgumentParser
@@ -73,7 +74,11 @@ class PyMOLAlignAll():
     def __init__(self, reference_object:str) -> None:
         self.ref = reference_object
         self.obj_list = self.non_sele_objs()
-        self.results_dict = {}
+        try:
+            with open(glob.glob("*results*.json")[0], 'r', encoding='UTF8') as results_json_file:
+                self.results_dict = json.load(results_json_file)
+        except IndexError:
+            self.results_dict = {}
         self.results_df = None
 
     def non_sele_objs(self):
@@ -84,14 +89,14 @@ class PyMOLAlignAll():
 
     def tmalign_all(self):
         for obj in self.obj_list:
-            if obj != self.ref:
+            if obj != self.ref and (obj not in self.results_dict or 'tmalign' not in self.results_dict[obj]):
                 tm = cmd.tmalign(obj, self.ref)
                 sub_results_dict = {'tmalign': tm}
                 self.results_dict.setdefault(obj, {}).update(sub_results_dict)
 
     def align_all(self):
         for obj in self.obj_list:
-            if obj != self.ref:
+            if obj != self.ref and (obj not in self.results_dict or 'align' not in self.results_dict[obj]):
                 print(f'\nAligning based on sequence (align):\t{obj}')
                 align_result_parser = PyMOLalign(cmd.align(obj, self.ref, cycles=10))
                 align_result_parser.show_results()
@@ -100,7 +105,7 @@ class PyMOLAlignAll():
 
     def cealign_all(self):
         for obj in self.obj_list:
-            if obj != self.ref:
+            if obj != self.ref and (obj not in self.results_dict or 'cealign' not in self.results_dict[obj]):
                 print(f'\nAligning based on structure (cealign):\t{obj}')
                 cealign_result_parser = PyMOLcealign(cmd.cealign(self.ref, obj))
                 cealign_result_parser.show_results()
@@ -109,7 +114,7 @@ class PyMOLAlignAll():
 
     def super_all(self):
         for obj in self.obj_list:
-            if obj != self.ref:
+            if obj != self.ref and (obj not in self.results_dict or 'super' not in self.results_dict[obj]):
                 print(f'\nSuperimposing (super):\t{obj}')
                 align_result_parser = PyMOLalign(cmd.super(obj, self.ref))
                 align_result_parser.show_results()
@@ -141,9 +146,10 @@ class PyMOLAlignAll():
         return self.results_df
 
 class TMalignMatrix():
-    def __init__(self, align_all_results:dict) -> None:
+    def __init__(self, align_all_results:dict, reference_object:str) -> None:
         self.results = align_all_results
-        self.tmmatrix = pd.DataFrame()
+        self.ref_obj = reference_object
+        self.tmmatrix = None
 
     def get_sequence_length(self, object_name):
         # Count the number of CA atoms in given object
@@ -151,13 +157,17 @@ class TMalignMatrix():
         return sequence_length
 
     def calculate_matrix(self, save_temp:bool):
+        cmd.reinitialize()
         # Generate a matrix of TMalign scores to build a structural dendrogram
         # Create a list of unique PDB codes ordered by their TMalign score
         model_list = [model for model, results in self.results.items()]
         unique_model_list = get_unique_models(model_list, top_x=100)
-
+        
+        for model in unique_model_list:
+            cmd.load(f'{model}.pdb')
+        
         try:
-            self.tmmatrix = pd.read_csv(glob.glob("*tmalign-matrix*.csv")[0], index_col=0)
+            self.tmmatrix = pd.read_csv(glob.glob(f"{self.ref_obj}-tmalign-matrix*.csv")[0], index_col=0)
         except IndexError:
             self.tmmatrix = pd.DataFrame(index=unique_model_list, columns=unique_model_list)
 
@@ -167,8 +177,14 @@ class TMalignMatrix():
             # This would be more efficient if storing values in a list/dict
             # Directly updating the dataframe makes the data easier to parse visually
             # The real bottleneck here is going to TMalign anyway so I'm leaving it
+        
+        pairs = combinations(unique_model_list, 2)
+        pairs_len = len(list(pairs))
+        pair_counter = 0
         for pair in combinations(unique_model_list, 2):
-            print(pair)
+            pair_counter +=1
+            print(f'\n{pair_counter}/{pairs_len}\n')
+            
             if isnan(self.tmmatrix.loc[pair[0], pair[1]]) or isnan(self.tmmatrix.loc[pair[1], pair[0]]):
                 # Set the smaller object as the reference (target) object
                 object_1_length = self.get_sequence_length(pair[0])
@@ -180,7 +196,7 @@ class TMalignMatrix():
                 elif object_1_length < object_2_length:
                     target = pair[0]
                     mobile = pair[1]
-                
+
                 tm = cmd.tmalign(mobile, target)
 
                 self.tmmatrix.loc[pair[0], pair[1]] = tm
@@ -191,7 +207,7 @@ class TMalignMatrix():
                 # Save results as they are calculated to cut down on recalculating TMalign scores
                 # after a crash or a restart
                 if save_temp:
-                    df_to_file(self.tmmatrix, filename='tmalign-matrix', filetype='csv')
+                    df_to_file(self.tmmatrix, filename=f'{self.ref_obj}-tmalign-matrix', filetype='csv')
 
         # Fill the diagonal with TMscores of 1 (always the score for identical proteins)
             # ∴ We do not need to run any calculations
@@ -280,36 +296,59 @@ def align():
     # Get the path for the user specified reference pdb/cif file
     cif_ref_path = user_args.reference.resolve()
 
-    # Load all pdb/cif files in cwd
+    # # Chunk size
+    # chunk_size = 500
+
+    # # Iterate over sections of CIFs up to 500 elements in length
+    # for i in range(0, len(cif_list), chunk_size):
+    #     chunk = cif_list[i:i+chunk_size]
+        
+    #     for cif in tqdm(chunk, desc='', unit='file'):
+    #         cmd.load(cif)
+
+    #         # Load all pdb/cif files in cwd
+    #     if cif_ref_path not in chunk:
+    #         cmd.load(cif_ref_path)
+
+    #     cmd.remove('resn hoh')
+    #     pymol_instance = PyMOLAlignAll(cif_ref_path.stem)
+    #     for alignment_algorithm in (pymol_instance.align_all,
+    #                                 pymol_instance.cealign_all,
+    #                                 pymol_instance.super_all,
+    #                                 pymol_instance.tmalign_all):
+
+    #         print('-' * 100)
+    #         alignment_algorithm()
+    #         with open(f'{pymol_instance.ref}-results.json', 'w', encoding='UTF8') as json_file:
+    #             json.dump(pymol_instance.results_dict, json_file, indent=4)
+
+    #     cmd.reinitialize()
+
+    for cif in tqdm(cif_list, desc='', unit='file'):
+        cmd.load(cif)
+
+        # Load all pdb/cif files in cwd
     if cif_ref_path not in cif_list:
         cmd.load(cif_ref_path)
 
-    for cif in cif_list:
-        print(f'Loading: {cif}')
-        cmd.load(cif)
-
-    # Remove waters
     cmd.remove('resn hoh')
-
-    # Initialise a PyMOL instance and set the reference object
     pymol_instance = PyMOLAlignAll(cif_ref_path.stem)
-
-    # Align all other objects to the reference using align, cealign, super and tmalign
     for alignment_algorithm in (pymol_instance.align_all,
                                 pymol_instance.cealign_all,
                                 pymol_instance.super_all,
                                 pymol_instance.tmalign_all):
-        try:
-            print('-' * 100)
-            alignment_algorithm()
-        except:
-            pass
+
+        print('-' * 100)
+        alignment_algorithm()
+        with open(f'{pymol_instance.ref}-results.json', 'w', encoding='UTF8') as json_file:
+            json.dump(pymol_instance.results_dict, json_file, indent=4)
+
 
     # Sort results_dict by TMalign score
     pymol_instance.results_dict = dict(sorted(pymol_instance.results_dict.items(), key=lambda item: item[1]['tmalign'], reverse=True))
 
     # Write results to a JSON file (mainly for testing)
-    with open('results.json', 'w', encoding='UTF8') as json_file:
+    with open(f'{pymol_instance.ref}-results.json', 'w', encoding='UTF8') as json_file:
         json.dump(pymol_instance.results_dict, json_file, indent=4)
 
     # Convert the results dictionary to a dataframe and write to csv and xlsx files
@@ -320,26 +359,23 @@ def align():
                                 index_label='PDB_code_with_chain',
                                 filetype=extension)
 
-
-    cmd.reinitialize()
-
     # Calculate TMalign score matrix
         # Used to create a structural dendrogram
 
-    tmmatrix = TMalignMatrix(pymol_instance.results_dict)
+    tmmatrix = TMalignMatrix(align_all_results=pymol_instance.results_dict, reference_object=pymol_instance.ref)
 
     tmmatrix.calculate_matrix(save_temp=True)
 
     for extension in ('csv', 'xlsx'):
         df_to_file(dataframe=tmmatrix.get_matrix(),
-                                filename='tmalign-matrix',
+                                filename=f'{tmmatrix.ref_obj}-tmalign-matrix',
                                 filetype=extension)
 
     tmmatrix.make_dendrogram()
 
-    return pymol_instance.ref, cif_ref_path, pymol_instance.results_dict
+    return cif_ref_path, pymol_instance.results_dict
 
-def visualise_top_x(results:dict, top_x:int, reference_obj:str):
+def visualise_top_x(results:dict, top_x:int, reference_obj:Path):
     # Create PyMOL sessions for the top x PDBs with the highest TMalign scores
     # Only 1 chain per PDB code is used (often there are identical chains in the same PDB) 
 
@@ -347,20 +383,21 @@ def visualise_top_x(results:dict, top_x:int, reference_obj:str):
     model_list = [model for model, results in results.items()]
     unique_model_list = get_unique_models(model_list, top_x)
 
+    cmd.reinitialize()
     # Open and align copies of the chain for each alignment method
     # PLACEHOLDER! DOES NOT WORK YET!
     for m in unique_model_list:
         cmd.load(reference_obj)
         # CHANGE: Name objects '{algorithm}_{rmsd/tm to 4 d.p.}
         for method in ('align', 'cealign', 'super', 'tmalign'):
-            cmd.load(f'{m}.pdb', f'{m}_{method}_{m}')
+            cmd.load(f'{m}.pdb', f'{method}')
 
         # print(cmd.get_names())
         cmd.remove('resn hoh')
-        cmd.cealign(reference_obj.stem, f'{m}_cealign')
-        cmd.align(f'{m}_align', reference_obj.stem)
-        cmd.super(f'{m}_super', reference_obj.stem)
-        cmd.tmalign(f'{m}_tmalign', reference_obj.stem)
+        cmd.cealign(reference_obj.stem, f'cealign')
+        cmd.align(f'align', reference_obj.stem)
+        cmd.super(f'super', reference_obj.stem)
+        cmd.tmalign(f'tmalign', reference_obj.stem)
 
         # 0.65 = 35% visible
         cmd.set('cartoon_transparency', 0.65, f'(all and not {reference_obj.stem})')
@@ -368,12 +405,12 @@ def visualise_top_x(results:dict, top_x:int, reference_obj:str):
         
         # Save pymol session file
         cmd.save(f'{m}.pse')
+        print(f'Session saved: {m}.pse')
         cmd.reinitialize()
 
 def main():
-    reference_obj, reference_obj_path, alignment_results = align()
-
-    visualise_top_x(alignment_results, 25, reference_obj_path)
+    reference_obj_path, alignment_results = align()
+    # visualise_top_x(alignment_results, 10, reference_obj_path)
 
 if __name__ == '__main__':
     main()
